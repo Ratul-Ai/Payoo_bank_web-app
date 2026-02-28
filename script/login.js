30bf4595cb4d84adc37b66fbb19112c67c28e64e
Ratul-Ai <ratul.bd.429@gmail.com>
2026-02-28 22:15:11 +0600

    .html

--- script/login.js
+++ script/login.js
@@ -11,7 +11,7 @@ inputPin.addEventListener("input", function () {
 document.getElementById("input-btn").addEventListener("click", function () {
     
     if(inputNumber.value==='01111111111' && inputPin.value==='2222'){
-        window.location.assign('home.html');
+        window.location.assign('/home');
         alert('Log in successful');
     }
     else{
